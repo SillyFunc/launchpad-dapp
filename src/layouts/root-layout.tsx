31051b746988817299eee67835a645fb920c3a35
SillyFunc <erickwok@tuta.io>
2026-08-30 01:15:58 +0800

    feat: add DotPattern component for interactive background effect

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -2,21 +2,20 @@ import { useState } from 'react'
 import { Outlet } from 'react-router'
 import Header from '../components/header'
 import Sidebar from '../components/sidebar'
+import DotPattern from '@/components/ui/dot-pattern'
 
 function RootLayout() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <div className="flex min-h-dvh flex-col bg-black text-white">
+    <div className="relative size-full bg-black flex min-h-dvh flex-col text-white">
       <Header
         isMenuOpen={isMenuOpen}
         onToggleMenu={() => setIsMenuOpen((prev) => !prev)}
       />
-      <Sidebar
-        isOpen={isMenuOpen}
-        onClose={() => setIsMenuOpen(false)}
-      />
-      <main className="mx-auto flex w-full flex-1 flex-col px-4">
+      <Sidebar isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+      <DotPattern dotSize={2} className="fixed inset-0" />
+      <main className="relative z-10 mx-auto flex w-full flex-1 flex-col px-4">
         <Outlet />
       </main>
     </div>
